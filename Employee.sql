19e31943911d9535bec90c2490e62c2d9e52cf8a
axb157030 <amine.benelbar.1@gmail.com>
2018-08-21 15:17:48 -0500

    Employee

---
+++ Employee.sql
@@ -0,0 +1,10 @@
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (1, "Rank10", "Project Manager", 2);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (2, "Rank9", "Coporate Manager", 2);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (3, "Rank8",  "General Manager", 2);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (4, "Rank7", "Senior Engineer", 1);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (5, "Rank6", "Entry Engineer", 1);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (6, "Rank5", "Accountaint", 1);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (7, "Rank4", "Financial Analysit", 1);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (8, "Rank3", "Janitor", 3);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (9, "Rank2", "Consultant", 1);
+insert into Employee (EmpID, Rank, Title, SupervisorID) values (10, "Rank1", "Lawyer", 3);
